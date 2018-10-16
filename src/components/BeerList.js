34ca425f80441f11a96a035dd20d71a33f8fbb6d
Thao Le <thuthao.le2809@gmail.com>
2018-10-16 17:10:41 +0700

    Search list

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -6,6 +6,7 @@ class BeerList extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            search:'',
             beers: []
         };
     }
@@ -23,38 +24,47 @@ class BeerList extends Component {
 
     componentDidMount() {
         this.fetchBeers();
-        // fetch("https://api.punkapi.com/v2/beers/")
-        //     .then(res => res.json())
-        //     .then(
-        //         results => {
-        //             this.setState({
-        //                 beers: results
-        //             })
-        //         },
-        //     )
     }
+
+    handleOnchange = (e) => {
+        this.setState({ search: e.target.value });
+    }
+
     render () {
-        const { beers } = this.state;
+        const { beers , search } = this.state;
+        // console.log(beers);
+        const filteredList = beers.filter(beer => {
+            return beer.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
+        })
         return(
-            <Router>
-                <div className="container mx-auto my-5 w-full">
-                    <h2 className="my-5 px-2">List of beers!</h2>
-                    <div className="list-beers flex flex-row">
-                        <ul className="list-reset w-1/3">
-                            {beers.map(beer=> (
-                                <li className="bg-black py-5 pl-3 shadow-md border-b-2" key={beer.id}>
-                                    <Link to={"/" + beer.id} className="no-underline text-white font-bold">{beer.name}</Link>
-                                </li>
-                                
-                            ))}
-                        </ul>
-                        <div className="w-2/3 bg-teal-lightest p-5">
-                            <h3>Description</h3>
-                            <Route exact path ={"/:id"} component={BeerItem}/>
+            <div>
+                <div className="container mx-auto m-5 text-center">
+                    <input 
+                        onChange={this.handleOnchange}
+                        value={this.state.search}
+                        type="text" placeholder="Search..." 
+                        className="border-2 border-solid w-1/2 h-12 rounded px-5 hover:shadow focus:border-none"/>
+                </div>
+                <Router>
+                    <div className="container mx-auto my-5 w-full">
+                        <h2 className="my-5 px-2">List of beers!</h2>
+                        <div className="list-beers flex flex-row">
+                            <ul className="list-reset w-1/3">
+                                {filteredList.length > 0 && filteredList.map(beer=> (
+                                    <li className="bg-black py-5 pl-3 shadow-md border-b-2" key={beer.id}>
+                                        <Link to={"/" + beer.id} className="no-underline text-white font-bold">{beer.name}</Link>
+                                    </li>
+                                    
+                                ))}
+                            </ul>
+                            <div className="w-2/3 bg-teal-lightest p-5">
+                                <h3>Description</h3>
+                                <Route exact path ={"/:id"} component={BeerItem}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Router>
+                </Router>
+            </div>
         );
     }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Search from './Search';
 import BeerList from './BeerList';
 
 class Main extends Component {
     render() {
         return (
             <main className="container mx-auto">
-                <Search/>
                 <BeerList/>
             </main>
         );
